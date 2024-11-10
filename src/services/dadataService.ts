1a875b0636c44c1de45f9e0e1c52d42d20d9d418
vak.vladislav <vlad.vakulenchikk@gmail.com>
2024-11-10 18:13:57 +0300

    add interceptors

--- src/services/dadataService.ts
+++ src/services/dadataService.ts
@@ -6,7 +6,12 @@ export const fetchAddressSuggestions = async (query: string) => {
   const response = await axios.post(
     'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address',
     { query, count: 5 },
-    { headers: { Authorization: `Token ${DADATA_API_KEY}` } },
   );
   return response.data.suggestions;
 };
+
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Token ${DADATA_API_KEY}`;
+
+  return config;
+});
